2e779731da42951151759d8a21585922dd14f302
Mark Bennett <mark.bennett@systemward.com>
2022-03-04 10:28:45 -0500

    Mark - remove local tfstate reference

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# terraform {
-#   backend "local" {
-#     path = "/your/path/to/terraform.tfstate"
-#   }
-# }
-
 terraform {
   backend "s3" {
     bucket = "tpg-training-remotestate"
