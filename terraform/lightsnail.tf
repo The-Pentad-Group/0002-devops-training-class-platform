c12ef7a2251e6656da014600a88d0c9c979b4bdf
Joshua Roman <josh.ray.roman@gmail.com>
2022-05-01 14:19:15 -0400

    Josh - add name to lightnsial config and resource block

--- terraform/lightsnail.tf
+++ terraform/lightsnail.tf
@@ -1,26 +1,27 @@
 resource "aws_lightsail_instance" "lightsnail_test" {
-    for_each = {
+  for_each = {
     for key, value in var.lightsnail_config :
     key => value
   }
+  name              = each.value.name
   availability_zone = each.value.availability_zone
   blueprint_id      = each.value.blueprint_id
   bundle_id         = each.value.bundle_id
- 
+
 }
 
-    # "blueprints": [
-    #     {
-    #         "blueprintId": "windows_server_2019",
-    #         "name": "Windows Server 2019",
-    #         "group": "windows_2019",
-    #         "type": "os",
-    #         "description": "Amazon Lightsail helps you build, deploy, scale, and manage Microsoft applications quickly, easily, and cost effectively with Windows Server 2019. For business IT applications, Lightsail runs Windows-based solutions in a secure, easily managed, and performant cloud environment. Common Windows use cases include Enterprise Windows-based application hosting, website and web-service hosting, data processing, distributed testing, ASP.NET application hosting, and running any other application requiring Windows software.",
-    #         "isActive": true,
-    #         "minPower": 0,
-    #         "version": "2022.04.13",
-    #         "versionCode": "1",
-    #         "productUrl": "https://aws.amazon.com/marketplace/pp/B07QZ4XZ8F",
-    #         "licenseUrl": "https://d7umqicpi7263.cloudfront.net/eula/product/ef297a90-3ad0-4674-83b4-7f0ec07c39bb/8dcaef51-8e20-41b3-a622-57864d247f86.txt",
-    #         "platform": "WINDOWS"
-    #     },
+# "blueprints": [
+#     {
+#         "blueprintId": "windows_server_2019",
+#         "name": "Windows Server 2019",
+#         "group": "windows_2019",
+#         "type": "os",
+#         "description": "Amazon Lightsail helps you build, deploy, scale, and manage Microsoft applications quickly, easily, and cost effectively with Windows Server 2019. For business IT applications, Lightsail runs Windows-based solutions in a secure, easily managed, and performant cloud environment. Common Windows use cases include Enterprise Windows-based application hosting, website and web-service hosting, data processing, distributed testing, ASP.NET application hosting, and running any other application requiring Windows software.",
+#         "isActive": true,
+#         "minPower": 0,
+#         "version": "2022.04.13",
+#         "versionCode": "1",
+#         "productUrl": "https://aws.amazon.com/marketplace/pp/B07QZ4XZ8F",
+#         "licenseUrl": "https://d7umqicpi7263.cloudfront.net/eula/product/ef297a90-3ad0-4674-83b4-7f0ec07c39bb/8dcaef51-8e20-41b3-a622-57864d247f86.txt",
+#         "platform": "WINDOWS"
+#     },
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -8,6 +8,7 @@ ec2_config = {
 }
 lightsnail_config = {
   JOSH_SNAIL = {
+  name              = "JOSH_SNAIL"  
   availability_zone = "us-east-1b"
   blueprint_id      = "windows_server_2019"
   bundle_id         = "micro_2_0"
